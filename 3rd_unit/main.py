c397825926ee9c6cbf0f6e52bea4ad96f4dc263b
pk.emelyanov <pk.emelyanov@tensor.ru>
2023-05-11 16:32:18 +0300

    2 task is done

--- 3rd_unit/main.py
+++ 3rd_unit/main.py
@@ -1,3 +1,10 @@
 
+listic = [1, 2, 3, 4, 5, 6, 12]
+min_elem = sorted(listic)[0]
+max_elem = sorted(listic, reverse=True)[0]
+average = round(sum(listic) / len(listic), 2)
+sum_list = sum(listic)
+
+
 
 
